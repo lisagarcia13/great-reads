de3f7a3e859fc5fdf1037cb953158d89f5e6843f
lisagarcia3 <lisa@Lisas-Air.attlocal.net>
2022-01-25 12:47:42 -0600

    fixed import error

--- back-end/greatreads/serializers.py
+++ back-end/greatreads/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from models import Author, Book
+from greatreads.models import Author, Book
 
 
 class BookSerializer(serializers.HyperlinkedModelSerializer):
