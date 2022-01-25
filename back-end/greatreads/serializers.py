569852b805f6be3f9156308fe19bff7394ec502a
lisagarcia3 <lisa@Lisas-Air.attlocal.net>
2022-01-25 10:33:58 -0600

    serializers created

---
+++ back-end/greatreads/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from models import Author, Book
+
+
+class BookSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Book
+        fields = ['title', 'release', 'description', 'author', 'image']
+
+
+class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+    books = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Author
+        fields = ['name', 'books']
