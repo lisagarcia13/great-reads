fc7324beba54ad98bc8eb9bb5948e4088685f2fc
lisagarcia3 <lisa@Lisas-Air.attlocal.net>
2022-01-25 15:32:53 -0600

    added permissions to views

--- back-end/greatreads/views.py
+++ back-end/greatreads/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from greatreads.serializers import BookSerializer, AuthorSerializer
 from greatreads.models import Book, Author
 
@@ -6,8 +6,10 @@ from greatreads.models import Book, Author
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
