9ceb44af0320bb16ce41806ba23b613cd8fe109f
lisagarcia3 <lisa@Lisas-Air.attlocal.net>
2022-01-26 09:43:58 -0600

    api token and refresh added to urls and settings

--- back-end/config/urls.py
+++ back-end/config/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from greatreads import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = routers.DefaultRouter()
 router.register('books', views.BookViewSet)
@@ -26,4 +27,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh', TokenRefreshView.as_view()),
 ]
