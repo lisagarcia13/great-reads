ac646addd6a12482932d6f63043096edce622478
lisagarcia3 <lisa@Lisas-Air.attlocal.net>
2022-01-27 13:30:44 -0600

    books and authors created in services

---
+++ front-end/client/src/services/authors.js
@@ -0,0 +1,46 @@
+import api from './apiConfig'
+
+export const getAuthors = async () => {
+  try {
+      const response = await api.get('/authors')
+      return response.data
+  } catch (error) {
+      throw error
+  }
+}
+
+export const getAuthor = async id => {
+  try {
+      const response = await api.get(`/authors/${id}`)
+      return response.data
+  } catch (error) {
+      throw error
+  }
+}
+
+export const createAuthor = async author => {
+  try {
+      const response = await api.post('/authors', author)
+      return response.data
+  } catch (error) {
+      throw error
+  }
+}
+
+export const updateAuthor = async (id, author) => {
+  try {
+      const response = await api.put(`/authors/${id}`, author)
+      return response.data
+  } catch (error) {
+      throw error
+  }
+}
+
+export const deleteAuthor = async id => {
+  try {
+      const response = await api.delete(`/authors/${id}`)
+      return response.data
+  } catch (error) {
+      throw error
+  }
+}
---
+++ front-end/client/src/services/books.js
@@ -0,0 +1,46 @@
+import api from './apiConfig'
+
+export const getBooks = async () => {
+  try {
+      const response = await api.get('/books')
+      return response.data
+  } catch (error) {
+      throw error
+  }
+}
+
+export const getBook = async id => {
+  try {
+      const response = await api.get(`/books/${id}`)
+      return response.data
+  } catch (error) {
+      throw error
+  }
+}
+
+export const createBook = async book => {
+  try {
+      const response = await api.post('/books', book)
+      return response.data
+  } catch (error) {
+      throw error
+  }
+}
+
+export const updateBook = async (id, book) => {
+  try {
+      const response = await api.put(`/books/${id}`, book)
+      return response.data
+  } catch (error) {
+      throw error
+  }
+}
+
+export const deleteBook = async id => {
+  try {
+      const response = await api.delete(`/books/${id}`)
+      return response.data
+  } catch (error) {
+      throw error
+  }
+}
