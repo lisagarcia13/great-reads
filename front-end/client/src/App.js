68b3e40fda8967633b30b938fff2d2b8ea201a97
lisagarcia3 <lisa@Lisas-Air.attlocal.net>
2022-01-27 12:55:29 -0600

    created routes

--- front-end/client/src/App.js
+++ front-end/client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path={'/'} element={<h1>HOME</h1>} />
+        <Route path={'/books'} element={<h1>ALL BOOKS</h1>} />
+        <Route path={'/books/:id'} element={<h1>BOOK DETAIL</h1>} />
+        <Route path={'/books/:id/edit'} element={<h1>BOOK EDIT</h1>} />
+        <Route path={'/add-books'} element={<h1>CREATE BOOK</h1>} />
+        <Route path={'/authors'} element={<h1>AUTHORS</h1>} />
+        <Route path={'/authors/:id'} element={<h1>AUTHOR DETAIL</h1>} />
+        <Route path={'/authors/:id/edit'} element={<h1>AUTHOR EDIT</h1>} />
+        <Route path={'/add-authors'} element={<h1> ADD AUTHORS</h1>} />
+        <Route path={'/sign-in'} element={<h1>SIGN IN</h1>} />
+        <Route path={'/sign-up'} element={<h1>SIGN UP</h1>} />
+        <Route path ={'/sign-out'} element={<h1>SIGN OUT</h1>} />
+      </Routes>
     </div>
   );
 }
