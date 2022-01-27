73ac9057f0844fded286b8f9e470965d093479d9
lisagarcia3 <lisa@Lisas-Air.attlocal.net>
2022-01-27 14:32:19 -0600

    navbar has working links

--- front-end/client/src/App.js
+++ front-end/client/src/App.js
@@ -11,11 +11,11 @@ function App() {
         <Route path={'/books'} element={<h1>ALL BOOKS</h1>} />
         <Route path={'/books/:id'} element={<h1>BOOK DETAIL</h1>} />
         <Route path={'/books/:id/edit'} element={<h1>BOOK EDIT</h1>} />
-        <Route path={'/add-books'} element={<h1>CREATE BOOK</h1>} />
+        <Route path={'/add-book'} element={<h1>CREATE BOOK</h1>} />
         <Route path={'/authors'} element={<h1>AUTHORS</h1>} />
         <Route path={'/authors/:id'} element={<h1>AUTHOR DETAIL</h1>} />
         <Route path={'/authors/:id/edit'} element={<h1>AUTHOR EDIT</h1>} />
-        <Route path={'/add-authors'} element={<h1> ADD AUTHORS</h1>} />
+        <Route path={'/add-author'} element={<h1> ADD AUTHORS</h1>} />
         <Route path={'/sign-in'} element={<h1>SIGN IN</h1>} />
         <Route path={'/sign-up'} element={<h1>SIGN UP</h1>} />
         <Route path ={'/sign-out'} element={<h1>SIGN OUT</h1>} />
--- front-end/client/src/components/Nav.jsx
+++ front-end/client/src/components/Nav.jsx
@@ -1,13 +1,51 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-export default function Nav() {
+const authenticatedOptions = (
+  <>
+    <NavLink className="link" to="/add-book">
+      Add Book
+    </NavLink>
+    <NavLink className="link" to="/add-author">
+      Add Author
+    </NavLink>
+    <NavLink className="link" to="/sign-out">
+      Sign Out
+    </NavLink>
+  </>
+);
+const unauthenticatedOptions = (
+  <>
+    <NavLink className="link" to="/sign-up">
+      Sign Up
+    </NavLink>
+    <NavLink className="link" to="/sign-in">
+      Sign In
+    </NavLink>
+  </>
+);
+const alwaysOptions = (
+  <>
+    <NavLink className="link" to="/books">
+      Books
+    </NavLink>
+    <NavLink className="link" to="/authors">
+      Authors
+    </NavLink>
+  </>
+);
+
+const Nav = ({ user }) => {
   return (
     <nav>
-      <div className="link-container">
-        <div>
-          <Link to="/books">Books</Link>
+      <div className="nav">
+        <NavLink to="/">GreatReads</NavLink>
+        <div className="links">
+          {user && <div className="link welcome">Welcome, {user.username}</div>}
+          {alwaysOptions}
+          {user ? authenticatedOptions : unauthenticatedOptions}
         </div>
       </div>
     </nav>
   );
-}
+};
+export default Nav;
