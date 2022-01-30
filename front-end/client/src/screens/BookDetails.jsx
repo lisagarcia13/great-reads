5dc236924d0f6e8f43c5b715fc4ffe1607ca160c
lisagarcia3 <lisa@Lisas-Air.attlocal.net>
2022-01-30 09:54:50 -0600

    book details css

--- front-end/client/src/screens/BookDetails.jsx
+++ front-end/client/src/screens/BookDetails.jsx
@@ -33,19 +33,26 @@ function BookDetails(props) {
   return (
     <Layout user={props.user}>
       <div className="book-detail-container">
+        <div className="book-img-flex">
+          <img className="book-detail-img" src={book.image} alt={book.title} />
+        </div>
         <div className="book-author">
-          <h2>{book.title}</h2>
-          <h4> Written by: {book.author}</h4>
+          <h2 className="book-title-detail">{book.title}</h2>
+          <h3> Written by: {book.author}</h3>
+          <div className="book-details">
+            <h4> Release Date: {book.release}</h4>
+            <h3>Description: </h3>
+            <span>
+              <p className="book-p">{book.description}</p>
+            </span>
+          </div>
         </div>
-        <img className="book-detail-img" src={book.image} alt={book.title} />
-        <div className="book-details">
-          <h4> Release Date: {book.release}</h4>
-          <h3>Description: </h3>
-          <p>{book.description}</p>
-        </div>
-        <div className="detail-button-container">
-          <Link to={`/books/${book.id}/edit`}>Edit</Link>
-          <button className="delete-button" onClick={handleDelete}>
+
+        <div className="book-detail-button-container">
+          <Link className="edit-book-link" to={`/books/${book.id}/edit`}>
+            Edit
+          </Link>
+          <button className="book-delete-button" onClick={handleDelete}>
             Delete
           </button>
         </div>
