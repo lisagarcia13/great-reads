97ca11a0e595d03852871fb444125a70c123d9d5
lisagarcia3 <lisa@Lisas-Air.attlocal.net>
2022-01-30 18:32:41 -0600

    books css

--- front-end/client/src/screens/Books.jsx
+++ front-end/client/src/screens/Books.jsx
@@ -1,10 +1,12 @@
 import BookCards from "../components/BookCards";
 import Layout from "../components/Layout";
+import "./Books.css";
 
 function Books({ books }, props) {
   return (
     <Layout user={props.user}>
       <div className="all-books">
+        <h2 className="book-header-main">Books</h2>
         <BookCards books={books} />
       </div>
     </Layout>
