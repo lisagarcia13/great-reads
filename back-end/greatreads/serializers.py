94b7fb2d6bced18c752ede5385db5f7861024f15
lisagarcia3 <lisa@Lisas-Air.attlocal.net>
2022-01-31 10:28:42 -0600

    updated serializer

--- back-end/greatreads/serializers.py
+++ back-end/greatreads/serializers.py
@@ -10,22 +10,9 @@ class BookSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['id', 'title', 'release', 'description', 'author', 'image']
 
 
-class AuthorSerializer(serializers.HyperlinkedModelSerializer):
-    books = BookSerializer(many=True)
+class AuthorSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Author
         fields = ['id', 'name', 'books', 'image']
-
-    def create(self, validated_data):
-        books_data = validated_data.pop('books')
-        author = Author.objects.create(**validated_data)
-        for book_data in books_data:
-            Book.objects.create(author=author, **book_data)
-        return author
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.image = validated_data.get('image', instance.image)
-        instance.save()
-        return instance
+        depth = 1
